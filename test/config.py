4e97b37d3e3bb3a8729c60b4472d5e3fb8db6c62
AAyachi328 <110423661+AAyachi328@users.noreply.github.com>
2022-08-10 13:20:08 +0200

    Update config.py

--- test/config.py
+++ test/config.py
@@ -9,12 +9,12 @@ class DefaultConfig:
     """Configuration for the bot."""
 
     PORT = 8000
-    APP_ID = os.environ.get("MicrosoftAppId")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword")
-    LUIS_APP_ID = os.environ.get("LuisAppId")
-    LUIS_API_KEY = os.environ.get("LuisAPIKey")
-    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName")
-    LUIS_API_ENDPOINT = os.environ.get("LuisAPIEndPoint")
+    APP_ID = os.environ.get("MicrosoftAppId","")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword","")
+    LUIS_APP_ID = os.environ.get("LuisAppId", "987333db-27f7-48f8-87ec-5be5e584df98")
+    LUIS_API_KEY = os.environ.get("LuisAPIKey", "02d3280f9d6f48a2ad7e532e95d09e4b")
+    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName","")
+    LUIS_API_ENDPOINT = os.environ.get("LuisAPIEndPoint","")
     APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
-        "AppInsightsInstrumentationKey"
+        "AppInsightsInstrumentationKey",""
     )
