8635d9611ea32921283b5efda8647b265a5df412
AAyachi328 <110423661+AAyachi328@users.noreply.github.com>
2022-08-10 13:40:53 +0200

    Update test_flyme.py

--- test/test_flyme.py
+++ test/test_flyme.py
@@ -10,7 +10,7 @@ client_runtime = LUISRuntimeClient(endpoint=CONFIG.LUIS_API_ENDPOINT, credential
 
 def test_greetings():
 
-    test_request = "Hello"
+    test_request = "Hi"
     test_response = client_runtime.prediction.resolve(CONFIG.LUIS_APP_ID, query=test_request)
 
     expected_intent = "GreetingsIntent"
@@ -19,7 +19,7 @@ def test_greetings():
 
 def test_order_travelt():
 
-    test_request = "I need to book a flight"
+    test_request = "I need to travel from Tunis"
     test_response = client_runtime.prediction.resolve(CONFIG.LUIS_APP_ID, query=test_request)
 
     expected_intent = "OrderTravelIntent"
@@ -28,7 +28,7 @@ def test_order_travelt():
 
 def test_origin_entity():
 
-    test_request = "I need a trip from Busan"
+    test_request = "I need a trip from London"
     test_response = client_runtime.prediction.resolve(CONFIG.LUIS_APP_ID, query=test_request)
 
     expected_origin = "busan"
@@ -40,10 +40,10 @@ def test_origin_entity():
 
 def test_destination_entity():
 
-    test_request = "I'd like to go to Caprica"
+    test_request = "I'd like to go to Paris"
     test_response = client_runtime.prediction.resolve(CONFIG.LUIS_APP_ID, query=test_request)
 
-    expected_destination = "caprica"
+    expected_destination = "paris"
     actual_destination = ""
     if test_response.entities[0].type == 'ArrivalCity':
         actual_destination = test_response.entities[0].entity
@@ -52,10 +52,10 @@ def test_destination_entity():
 
 def test_order_travel_intent_budget_entity():
 
-    test_request = "I'd like to book a trip and I have a budget of 1000 usd"
+    test_request = "I can travel for a budget of 500 dollars"
     test_response = client_runtime.prediction.resolve(CONFIG.LUIS_APP_ID, query=test_request)
 
-    expected_budget = "1000 usd"
+    expected_budget = "500"
     actual_budget = ""
     if test_response.entities[0].type == 'Price':
         actual_budget = test_response.entities[0].entity
