c23aea40e527b0d821a7d5cd41fc10b981f2bf15
AAyachi328 <110423661+AAyachi328@users.noreply.github.com>
2022-08-10 13:25:17 +0200

    Update config.py

--- test/config.py
+++ test/config.py
@@ -13,8 +13,8 @@ class DefaultConfig:
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword","")
     LUIS_APP_ID = os.environ.get("LuisAppId", "987333db-27f7-48f8-87ec-5be5e584df98")
     LUIS_API_KEY = os.environ.get("LuisAPIKey", "02d3280f9d6f48a2ad7e532e95d09e4b")
-    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName","")
-    LUIS_API_ENDPOINT = os.environ.get("LuisAPIEndPoint","")
+    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "westeurope.api.cognitive.microsoft.com")
+    LUIS_API_ENDPOINT = os.environ.get("LuisAPIEndPoint", "https://ayachip10luis.cognitiveservices.azure.com/")
     APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
         "AppInsightsInstrumentationKey","xx"
     )
